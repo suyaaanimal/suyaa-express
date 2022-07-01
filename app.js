2f51ea4c525ff25d25d30a85062c4a1189aab612
sanamiy <sanamiev@gmail.com>
2022-07-01 11:13:34 +0900

    User登録&ログイン機能作成
    - userをmongodbに登録
    - セッションを保存
    
    これで問題なく動くのか不明

--- app.js
+++ app.js
@@ -1,6 +1,36 @@
 const express = require('express')
+const passport = require('passport');
+const mongoose = require('mongoose');
+
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+
 const app = express()
 const port = process.env.PORT || 3000
+
+const dbUrl = "mongodb://localhost/suyaa-express-db"
+mongoose.Promise = global.Promise;
+mongoose.connect(dbUrl);
+
+app.use(session({
+    secret: "secret word",
+    saveUninitialized: true,
+    resave: true,
+    store: MongoStore.create({
+      mongoUrl: dbUrl,
+    })
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/',  require('./routes/users'))
+
+// app.use(function(req,res,next){
+//     res.locals = {
+//         user: req.user
+//     };
+//     next();
+// });
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -9,3 +39,5 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+module.exports = app;
---
+++ routes/users.js
@@ -0,0 +1,48 @@
+var User = require('../models/users')
+const express = require('express')
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const router = express.Router()
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+// https://knimon-software.github.io/www.passportjs.org/guide/
+
+router.post('/signin',
+  passport.authenticate('local'),
+  function(req, res) {
+    res.redirect('/');
+});
+
+router.post('/signup', function(req, res, next){
+    User.register(
+        new User({ username: req.body.username, password: req.body.password }),
+        req.body.password,
+        function(err, user){
+            if (err) {
+                return res.render('user/signup', {title: 'User Sign up Error', user: req.body, error: err});
+            }
+            var authenticate = User.authenticate();
+
+            authenticate(req.body.username, req.body.password, function(err, result) {
+                if(err) {
+                    return res.render('user/signup', {title: 'User Sign up Error', user: req.body, error: err});
+                }
+                res.render('user/index',{title:'User Signed in', result: result});
+            });
+        }
+    );
+});
+
+app.post('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+
+
+module.exports = router
+
