c94ea4e72e03afee913c6425edfe8ce3e3204ec7
yawn <69970183+yawn-c111@users.noreply.github.com>
2022-07-02 21:21:39 +0900

    fix: network

--- hardhat.config.js
+++ hardhat.config.js
@@ -20,7 +20,7 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  module.exports = {
   solidity: "0.8.4",
   networks: {
-    rinkeby: {
+    mumbai: {
       url: process.env.API_URL,
       accounts: [process.env.PRIVATE_KEY]
     }
