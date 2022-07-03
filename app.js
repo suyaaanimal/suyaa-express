b9b457d6d6153eb9b6bc304cebfb510fdf42c3d9
atahatah <ratdotl.hata@gmail.com>
2022-07-03 16:44:36 +0900

    data

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-const express = require('express')
-const passport = require('passport');
-const bodyParser = require('body-parser');
+const express = require("express");
+const passport = require("passport");
+const bodyParser = require("body-parser");
 
-const app = express()
+const app = express();
 
 app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: true }))
 app.use(passport.initialize());
 
-app.use('/', require('./routes/auth'))
+app.use("/", require("./routes/auth"));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.get("/testdata", (req, res) => {
   // テストデータ
@@ -21,38 +21,179 @@ app.get("/testdata", (req, res) => {
     data: {
       totalScore: [
         // efficiency
-        { dateTime: "2022-6-29", score: 20 },
-        { dateTime: "2022-6-30", score: 30 },
-        { dateTime: "2022-7-1", score: 40 },
+        { dateTime: "2022-06-01", score: 20 },
+        { dateTime: "2022-06-02", score: 20 },
+        { dateTime: "2022-06-03", score: 20 },
+        { dateTime: "2022-06-04", score: 20 },
+        { dateTime: "2022-06-05", score: 20 },
+        { dateTime: "2022-06-06", score: 20 },
+        { dateTime: "2022-06-07", score: 20 },
       ],
       levels: [
-        { dateTime: "2022-6-29 23:0", level: 0 }, // level: wake
-        { dateTime: "2022-6-29 23:30", level: 1 }, //level: rem
-        { dateTime: "2022-6-30 1:0", level: 1 },
-        { dateTime: "2022-6-30 1:30", level: 1 },
-        { dateTime: "2022-6-30 2:0", level: 2 }, // level: light
-        { dateTime: "2022-6-30 2:30", level: 2 },
-        { dateTime: "2022-6-30 3:0", level: 2 },
-        { dateTime: "2022-6-30 3:30", level: 3 }, // level: deep
-        { dateTime: "2022-6-30 4:0", level: 2 },
-        { dateTime: "2022-6-30 4:30", level: 1 },
-        { dateTime: "2022-6-30 5:0", level: 2 },
-        { dateTime: "2022-6-30 5:30", level: 3 },
-        { dateTime: "2022-6-30 6:0", level: 2 },
-        { dateTime: "2022-6-30 6:30", level: 2 },
-        { dateTime: "2022-6-30 7:0", level: 1 },
-        { dateTime: "2022-6-30 7:30", level: 0 },
-        { dateTime: "2022-6-30 23:0", level: 0 }, // level: wake
-        { dateTime: "2022-6-30 23:30", level: 1 }, //level: rem
-        { dateTime: "2022-7-1 0:0", level: 1 },
+        { dateTime: "2022-06-01 19:00", level: 3 },
+        { dateTime: "2022-06-01 19:30", level: 1 },
+        { dateTime: "2022-06-01 20:00", level: 1 },
+        { dateTime: "2022-06-01 20:30", level: 2 },
+        { dateTime: "2022-06-01 21:00", level: 2 },
+        { dateTime: "2022-06-01 21:30", level: 3 },
+        { dateTime: "2022-06-01 22:00", level: 0 },
+        { dateTime: "2022-06-01 22:30", level: 2 },
+        { dateTime: "2022-06-01 23:00", level: 3 },
+        { dateTime: "2022-06-01 23:30", level: 1 },
+        { dateTime: "2022-06-02 00:00", level: 0 },
+        { dateTime: "2022-06-02 00:30", level: 1 },
+        { dateTime: "2022-06-02 01:00", level: 0 },
+        { dateTime: "2022-06-02 01:30", level: 2 },
+        { dateTime: "2022-06-02 02:00", level: 3 },
+        { dateTime: "2022-06-02 02:30", level: 3 },
+        { dateTime: "2022-06-02 03:00", level: 3 },
+        { dateTime: "2022-06-02 03:30", level: 1 },
+        { dateTime: "2022-06-02 04:00", level: 3 },
+        { dateTime: "2022-06-02 04:30", level: 3 },
+        { dateTime: "2022-06-02 05:00", level: 3 },
+        { dateTime: "2022-06-02 05:30", level: 1 },
+        { dateTime: "2022-06-02 06:00", level: 1 },
+        { dateTime: "2022-06-02 06:30", level: 1 },
+        { dateTime: "2022-06-02 07:00", level: 3 },
+        { dateTime: "2022-06-02 07:30", level: 1 },
+        { dateTime: "2022-06-02 19:00", level: 3 },
+        { dateTime: "2022-06-02 19:30", level: 2 },
+        { dateTime: "2022-06-02 20:00", level: 3 },
+        { dateTime: "2022-06-02 20:30", level: 3 },
+        { dateTime: "2022-06-02 21:00", level: 1 },
+        { dateTime: "2022-06-02 21:30", level: 2 },
+        { dateTime: "2022-06-02 22:00", level: 3 },
+        { dateTime: "2022-06-02 22:30", level: 3 },
+        { dateTime: "2022-06-02 23:00", level: 2 },
+        { dateTime: "2022-06-02 23:30", level: 1 },
+        { dateTime: "2022-06-03 00:00", level: 3 },
+        { dateTime: "2022-06-03 00:30", level: 1 },
+        { dateTime: "2022-06-03 01:00", level: 3 },
+        { dateTime: "2022-06-03 01:30", level: 1 },
+        { dateTime: "2022-06-03 02:00", level: 3 },
+        { dateTime: "2022-06-03 02:30", level: 3 },
+        { dateTime: "2022-06-03 03:00", level: 1 },
+        { dateTime: "2022-06-03 03:30", level: 0 },
+        { dateTime: "2022-06-03 04:00", level: 1 },
+        { dateTime: "2022-06-03 04:30", level: 1 },
+        { dateTime: "2022-06-03 05:00", level: 3 },
+        { dateTime: "2022-06-03 05:30", level: 3 },
+        { dateTime: "2022-06-03 06:00", level: 2 },
+        { dateTime: "2022-06-03 06:30", level: 1 },
+        { dateTime: "2022-06-03 07:00", level: 1 },
+        { dateTime: "2022-06-03 07:30", level: 1 },
+        { dateTime: "2022-06-03 19:00", level: 3 },
+        { dateTime: "2022-06-03 19:30", level: 3 },
+        { dateTime: "2022-06-03 20:00", level: 3 },
+        { dateTime: "2022-06-03 20:30", level: 2 },
+        { dateTime: "2022-06-03 21:00", level: 0 },
+        { dateTime: "2022-06-03 21:30", level: 3 },
+        { dateTime: "2022-06-03 22:00", level: 2 },
+        { dateTime: "2022-06-03 22:30", level: 3 },
+        { dateTime: "2022-06-03 23:00", level: 2 },
+        { dateTime: "2022-06-03 23:30", level: 2 },
+        { dateTime: "2022-06-04 00:00", level: 1 },
+        { dateTime: "2022-06-04 00:30", level: 3 },
+        { dateTime: "2022-06-04 01:00", level: 1 },
+        { dateTime: "2022-06-04 01:30", level: 1 },
+        { dateTime: "2022-06-04 02:00", level: 1 },
+        { dateTime: "2022-06-04 02:30", level: 0 },
+        { dateTime: "2022-06-04 03:00", level: 0 },
+        { dateTime: "2022-06-04 03:30", level: 1 },
+        { dateTime: "2022-06-04 04:00", level: 2 },
+        { dateTime: "2022-06-04 04:30", level: 1 },
+        { dateTime: "2022-06-04 05:00", level: 3 },
+        { dateTime: "2022-06-04 05:30", level: 1 },
+        { dateTime: "2022-06-04 06:00", level: 1 },
+        { dateTime: "2022-06-04 06:30", level: 2 },
+        { dateTime: "2022-06-04 07:00", level: 3 },
+        { dateTime: "2022-06-04 07:30", level: 0 },
+        { dateTime: "2022-06-04 19:00", level: 1 },
+        { dateTime: "2022-06-04 19:30", level: 1 },
+        { dateTime: "2022-06-04 20:00", level: 3 },
+        { dateTime: "2022-06-04 20:30", level: 2 },
+        { dateTime: "2022-06-04 21:00", level: 1 },
+        { dateTime: "2022-06-04 21:30", level: 1 },
+        { dateTime: "2022-06-04 22:00", level: 1 },
+        { dateTime: "2022-06-04 22:30", level: 3 },
+        { dateTime: "2022-06-04 23:00", level: 1 },
+        { dateTime: "2022-06-04 23:30", level: 3 },
+        { dateTime: "2022-06-05 00:00", level: 3 },
+        { dateTime: "2022-06-05 00:30", level: 2 },
+        { dateTime: "2022-06-05 01:00", level: 0 },
+        { dateTime: "2022-06-05 01:30", level: 3 },
+        { dateTime: "2022-06-05 02:00", level: 1 },
+        { dateTime: "2022-06-05 02:30", level: 2 },
+        { dateTime: "2022-06-05 03:00", level: 0 },
+        { dateTime: "2022-06-05 03:30", level: 2 },
+        { dateTime: "2022-06-05 04:00", level: 3 },
+        { dateTime: "2022-06-05 04:30", level: 1 },
+        { dateTime: "2022-06-05 05:00", level: 2 },
+        { dateTime: "2022-06-05 05:30", level: 0 },
+        { dateTime: "2022-06-05 06:00", level: 1 },
+        { dateTime: "2022-06-05 06:30", level: 1 },
+        { dateTime: "2022-06-05 07:00", level: 3 },
+        { dateTime: "2022-06-05 07:30", level: 3 },
+        { dateTime: "2022-06-05 19:00", level: 1 },
+        { dateTime: "2022-06-05 19:30", level: 2 },
+        { dateTime: "2022-06-05 20:00", level: 1 },
+        { dateTime: "2022-06-05 20:30", level: 1 },
+        { dateTime: "2022-06-05 21:00", level: 2 },
+        { dateTime: "2022-06-05 21:30", level: 0 },
+        { dateTime: "2022-06-05 22:00", level: 1 },
+        { dateTime: "2022-06-05 22:30", level: 3 },
+        { dateTime: "2022-06-05 23:00", level: 1 },
+        { dateTime: "2022-06-05 23:30", level: 3 },
+        { dateTime: "2022-06-06 00:00", level: 3 },
+        { dateTime: "2022-06-06 00:30", level: 2 },
+        { dateTime: "2022-06-06 01:00", level: 2 },
+        { dateTime: "2022-06-06 01:30", level: 3 },
+        { dateTime: "2022-06-06 02:00", level: 3 },
+        { dateTime: "2022-06-06 02:30", level: 3 },
+        { dateTime: "2022-06-06 03:00", level: 0 },
+        { dateTime: "2022-06-06 03:30", level: 1 },
+        { dateTime: "2022-06-06 04:00", level: 2 },
+        { dateTime: "2022-06-06 04:30", level: 3 },
+        { dateTime: "2022-06-06 05:00", level: 3 },
+        { dateTime: "2022-06-06 05:30", level: 0 },
+        { dateTime: "2022-06-06 06:00", level: 1 },
+        { dateTime: "2022-06-06 06:30", level: 2 },
+        { dateTime: "2022-06-06 07:00", level: 2 },
+        { dateTime: "2022-06-06 07:30", level: 2 },
+        { dateTime: "2022-06-06 19:00", level: 2 },
+        { dateTime: "2022-06-06 19:30", level: 1 },
+        { dateTime: "2022-06-06 20:00", level: 2 },
+        { dateTime: "2022-06-06 20:30", level: 3 },
+        { dateTime: "2022-06-06 21:00", level: 3 },
+        { dateTime: "2022-06-06 21:30", level: 1 },
+        { dateTime: "2022-06-06 22:00", level: 3 },
+        { dateTime: "2022-06-06 22:30", level: 3 },
+        { dateTime: "2022-06-06 23:00", level: 3 },
+        { dateTime: "2022-06-06 23:30", level: 1 },
+        { dateTime: "2022-06-07 00:00", level: 2 },
+        { dateTime: "2022-06-07 00:30", level: 3 },
+        { dateTime: "2022-06-07 01:00", level: 2 },
+        { dateTime: "2022-06-07 01:30", level: 1 },
+        { dateTime: "2022-06-07 02:00", level: 3 },
+        { dateTime: "2022-06-07 02:30", level: 2 },
+        { dateTime: "2022-06-07 03:00", level: 3 },
+        { dateTime: "2022-06-07 03:30", level: 3 },
+        { dateTime: "2022-06-07 04:00", level: 3 },
+        { dateTime: "2022-06-07 04:30", level: 2 },
+        { dateTime: "2022-06-07 05:00", level: 3 },
+        { dateTime: "2022-06-07 05:30", level: 2 },
+        { dateTime: "2022-06-07 06:00", level: 3 },
+        { dateTime: "2022-06-07 06:30", level: 3 },
+        { dateTime: "2022-06-07 07:00", level: 2 },
+        { dateTime: "2022-06-07 07:30", level: 3 },
       ],
     },
   });
 });
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`App started at http://localhost:${port}`)
-})
+  console.log(`App started at http://localhost:${port}`);
+});
 
 module.exports = app;
